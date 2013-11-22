d197b653e9f47c4d7083c51338c99dcc92e599e6
jhonny <steven.alvarez.v@gmail.com>
2013-11-22 18:04:07 -0400

    asdfasdf

--- js/main.js
+++ js/main.js
@@ -67,3 +67,40 @@ $( window ).on( "orientationchange", function( event ) {
         });
     } 
 });
+
+//--------------- watchPosition --------------------
+
+var watchID;
+var geoLoc;
+
+function showLocation(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  alert("Latitude : " + latitude + " Longitude: " + longitude);
+}
+
+function errorHandler(err) {
+  if(err.code == 1) {
+    alert("Error: Access is denied!");
+  }else if( err.code == 2) {
+    alert("Error: Position is unavailable!");
+  }
+}
+function getLocationUpdate(){
+
+   if(navigator.geolocation){
+      // timeout at 15000 milliseconds (15 seconds)
+      var options = {timeout:15000};
+      geoLoc = navigator.geolocation;
+      watchID = geoLoc.watchPosition(showLocation, 
+                                     errorHandler,
+                                     options);
+   }else{
+      alert("Sorry, browser does not support geolocation!");
+   }
+}
+function stopWatch(){
+   geoLoc.clearWatch(watchID);
+}
+
+getLocationUpdate();
